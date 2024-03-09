d7bf2153130873772205b5b59f2d91be983b109a
José Javier Serrano Solís <82909271+JavierSerranoRCK@users.noreply.github.com>
2024-03-08 19:54:22 -0600

    Imprimir en start de codigo

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -7,7 +7,7 @@ public class Example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ABC");   
     }
 
     // Update is called once per frame
