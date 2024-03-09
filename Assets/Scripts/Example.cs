24a84543ec6918f30462a67adaf82a9ad627a9bf
José Javier Serrano Solís <82909271+JavierSerranoRCK@users.noreply.github.com>
2024-03-08 19:55:48 -0600

    ejemplo nuevo

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -7,7 +7,7 @@ public class Example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("EFG");   
     }
 
     // Update is called once per frame
